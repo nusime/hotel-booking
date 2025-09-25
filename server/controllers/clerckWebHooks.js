7fbf75222ee8090db5f15c31607919ca3a9b5d0c
Nusit <154251667+nusit@users.noreply.github.com>
2025-09-25 21:50:55 +0300

    Initial backend setup with Express, CORS, and environment config

---
+++ server/controllers/clerckWebHooks.js
@@ -0,0 +1,57 @@
+import User from "../models/User.js";
+import { Webhook } from "svix";
+
+const clerkWebhooks = async (req, res) => {
+    try {
+
+        // Create a svix webhook instance
+        const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
+
+        // Getting header
+        const headers = {
+            'svix-id': req.headers['svix-id'],
+            'svix-timestamp': req.headers['svix-timestamp'],
+            'svix-signature': req.headers['svix-signature']
+        };
+
+        // Verifying headers
+        await whook.verify(JSON.stringify(req.body), headers);
+
+        // Getting data from request body
+        const {data, type} = req.body;
+
+        const userData = {
+            _id: data.id,
+            email: data.email_addresses[0].email_addresses,
+            username: data.first_name + ' ' + data.last_name,
+            image: data.image_url,
+        }
+
+        // Switch case for different event types
+        switch (type) {
+
+            case 'user.created': {
+                await User.create(userData);
+                break;
+            }
+
+            case 'user.updated': {
+                await User.findByIdAndUpdate(data.id, userData);
+                break;
+            }
+            case 'user.deleted': {
+                await User.findByIdAndDelete(data.id);
+                break;
+            } 
+        
+            default:
+                break;
+        }
+        res.json({success: true, message: 'Webhook received'});
+    } catch (error) {
+        console.log(error.message);
+        res.json({success: false, message: error.message});
+    }
+}
+
+export default clerkWebhooks;
---
+++ server/server.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import connectDB from './configs/db.js';
+import { clerkMiddleware } from '@clerk/express';
+import clerkWebhooks from './controllers/clerckWebHooks.js';
+
+connectDB();
+
+const app = express();
+app.use(cors());
+
+// Clerk middleware
+app.use(express.json());
+app.use(clerkMiddleware());
+
+// API to listen to clerk webhooks
+app.use('/api/clerk', clerkWebhooks);
+
+app.get('/', (req, res) => res.send('API is working'));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
