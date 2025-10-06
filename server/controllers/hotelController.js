bcdebfc419a653cbb473bd8b71800ee045da6b8e
Nusit <154251667+nusit@users.noreply.github.com>
2025-10-06 21:36:18 +0300

    add and update controllers for booking, hotel, room, and user

---
+++ server/controllers/hotelController.js
@@ -0,0 +1,29 @@
+import Hotel from "../models/Hotel.js";
+import User from "../models/User.js";
+
+export const registerHotel = async (req, res) => {
+    try {
+        const {name, address, contact, city} = req.body;
+        const owner = req.user._id;
+
+        // Check if the user is already registered
+        const hotel = await Hotel.findOne({owner});
+        if (hotel) {
+            return res.status(400).json({success: false, message: 'Hotel already registered'});
+        }
+
+        await Hotel.create({
+            name,
+            address,
+            contact,
+            city,
+            owner
+        });
+
+        await User.findByIdAndUpdate(owner, {role: 'hotelOwner'});
+
+        res.status(201).json({success: true, message: 'Hotel registered successfully'});
+    } catch (error) {
+        res.status(500).json({success: false, message: error.message});
+    }
+}
